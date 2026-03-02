a251961e6a7d54d7631f6e6c1a2d54e96d2de6a2
YogashreeJ <yj2208@srmist.edu.in>
2026-03-02 19:43:00 +0530

    chore:project setup with base architecture structure

---
+++ medledger/src/main/java/com/yoga/medledger/MedledgerApplication.java
@@ -0,0 +1,13 @@
+package com.yoga.medledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedledgerApplication.class, args);
+	}
+
+}
---
+++ medledger/src/test/java/com/yoga/medledger/MedledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoga.medledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
