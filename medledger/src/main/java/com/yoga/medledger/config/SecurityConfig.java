5eb5315f2a02021e5a6d66e572dc20174d3e166b
YogashreeJ <yj2208@srmist.edu.in>
2026-03-06 17:40:44 +0530

    feat: integrate Spring Security with database using UserDetailsService

--- medledger/src/main/java/com/yoga/medledger/config/SecurityConfig.java
+++ medledger/src/main/java/com/yoga/medledger/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.yoga.medledger.config;
 
+import com.yoga.medledger.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    private final CustomUserDetailsService customUserDetailsService;
 
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -15,5 +21,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return customUserDetailsService;
     }
 }
